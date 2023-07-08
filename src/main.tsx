905438748175b86a4d0038ea5521640cd060ab78
hamdi-karim <k.hamdi1995@gmail.com>
2023-07-08 18:58:55 +0100

    deploy setting

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,15 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
-import App from "./App"
-import "bootstrap/dist/css/bootstrap.min.css"
-import { BrowserRouter } from "react-router-dom"
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+
+import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.DEV ? "/" : "/essa-signature/"}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
-)
+);
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()]
-})
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react()],
+    base: "/",
+  };
+
+  if (command !== "serve") {
+    config.base = "/essa-signature/";
+  }
+
+  return config;
+});
